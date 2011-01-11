1eb7596576d04ccfa6717555f50c07a5bb27d5d0
Daichi Hiroki <hirokidaichi@gmail.com>
2011-01-11 20:26:10 +0900

    Make

--- Makefile
+++ Makefile
@@ -1,32 +1,43 @@
 
 
 CORE = ./build/brook-core.js
-FULL = ./build/brook.js
+COMPAT = ./build/brook-compat.js
 MIN  = ./build/brook.min.js
+MOBILE = ./build/brook-mobile.js
 
-all : $(CORE) $(FULL) $(MIN)
+all : $(CORE) $(COMPAT) $(MIN)
 
 clean :
 	rm -rf ./build/*
 
 $(CORE) : \
-	./lib/array.js \
 	./src/brook.js \
 	./src/brook/util.js \
+	./src/brook/lang/array.js\
+	./src/brook/lang/class.js\
+	./src/brook/lang/object.js\
+	./src/brook/lang/string.js \
 	./src/brook/lamda.js\
 	./src/brook/channel.js\
 	./src/brook/model.js 
 	cat $^ > $@
 
 
-$(FULL) : \
+$(COMPAT) : \
 	$(CORE) \
 	./src/brook/dom/compat.js\
 	./src/brook/dom/gateway.js\
 	./src/brook/widget.js 
 	cat $^ > $@
 
+$(MOBILE) : \
+	$(COMPAT) \
+	./src/brook/mobile/dom/event.js \
+	./src/brook/mobile/net/httprequester.js \
+	./src/brook/mobile/net/jsonrpc.js
+	cat $^ > $@
+
 $(MIN) : \
-	$(FULL)
+	$(COMPAT)
 	perl ./bin/minify < $^ > $@
 
--- build/brook-core.js
+++ build/brook-core.js
@@ -1,230 +1,3 @@
-if (!Array.prototype.forEach)
-{
-  Array.prototype.forEach = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this)
-        fun.call(thisp, this[i], i, this);
-    }
-  };
-}
-
-if (!Array.prototype.reduce)
-{
-  Array.prototype.reduce = function(fun /*, initialValue */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun !== "function")
-      throw new TypeError();
-
-    // no value to return if no initial value and an empty array
-    if (len == 0 && arguments.length == 1)
-      throw new TypeError();
-
-    var k = 0;
-    var accumulator;
-    if (arguments.length >= 2)
-    {
-      accumulator = arguments[1];
-    }
-    else
-    {
-      do
-      {
-        if (k in t)
-        {
-          accumulator = t[k++];
-          break;
-        }
-
-        // if array contains no values, no initial value to return
-        if (++k >= len)
-          throw new TypeError();
-      }
-      while (true);
-    }
-
-    while (k < len)
-    {
-      if (k in t)
-        accumulator = fun.call(undefined, accumulator, t[k], k, t);
-      k++;
-    }
-
-    return accumulator;
-  };
-}
-
-if (!Array.prototype.map)
-{
-  Array.prototype.map = function(fun /*, thisp */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun !== "function")
-      throw new TypeError();
-
-    var res = new Array(len);
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in t)
-        res[i] = fun.call(thisp, t[i], i, t);
-    }
-
-    return res;
-  };
-}
-
-if (!Array.prototype.every)
-{
-  Array.prototype.every = function(fun /*, thisp */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun !== "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in t && !fun.call(thisp, t[i], i, t))
-        return false;
-    }
-
-    return true;
-  };
-}
-
-if (!Array.prototype.filter)
-{
-  Array.prototype.filter = function(fun /*, thisp */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun !== "function")
-      throw new TypeError();
-
-    var res = [];
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in t)
-      {
-        var val = t[i]; // in case fun mutates this
-        if (fun.call(thisp, val, i, t))
-          res.push(val);
-      }
-    }
-
-    return res;
-  };
-}
-
-if (!Array.prototype.some)
-{
-  Array.prototype.some = function(fun /*, thisp */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun !== "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in t && fun.call(thisp, t[i], i, t))
-        return true;
-    }
-
-    return false;
-  };
-}
-
-if (!Array.prototype.reduceRight)
-{
-  Array.prototype.reduceRight = function(callbackfn /*, initialValue */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof callbackfn !== "function")
-      throw new TypeError();
-
-    // no value to return if no initial value, empty array
-    if (len === 0 && arguments.length === 1)
-      throw new TypeError();
-
-    var k = len - 1;
-    var accumulator;
-    if (arguments.length >= 2)
-    {
-      accumulator = arguments[1];
-    }
-    else
-    {
-      do
-      {
-        if (k in this)
-        {
-          accumulator = this[k--];
-          break;
-        }
-
-        // if array contains no values, no initial value to return
-        if (--k < 0)
-          throw new TypeError();
-      }
-      while (true);
-    }
-
-    while (k >= 0)
-    {
-      if (k in t)
-        accumulator = callbackfn.call(undefined, accumulator, t[k], k, t);
-      k--;
-    }
-
-    return accumulator;
-  };
-}
-
 Namespace('brook').define(function(ns){
     var VERSION = "0.01";
     var Promise = function(next){
@@ -407,6 +180,25 @@ Namespace('brook.util')
 
 
 
+Namespace('brook.lang.class')
+.define(function(ns){
+    ns.provide({
+        defineClass: function() {
+            var properties = $A(arguments);
+            var klass = function() {
+                this.initialize.apply(this, arguments);
+            }
+            for (var i = 0, l = properties.length; i < l; i++)
+                for (var property in properties[i]) 
+                    klass.prototype[property] = properties[i][property];
+
+            if (!klass.prototype.initialize)
+                klass.prototype.initialize = function(){};
+            klass.prototype.constructor = klass;
+            return klass;
+        }
+    })
+});
 Namespace('brook.lamda')
 .define(function(ns){
     var cache = {};
